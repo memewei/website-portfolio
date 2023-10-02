3fc5860cc66440f6d898772d32fb4a586c02e506
memewei <minwei2002@gmail.com>
2023-10-02 20:05:07 +0800

    Fix qualification timeline

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const navMenu = document.querySelector(".nav-menu");
 const navToggle = document.querySelector(".nav-toggle");
 const navClose = document.querySelector(".nav-close");
 const navLink = document.querySelectorAll(".nav-link");
-const skillsContent = document.querySelector(".skills-content");
+const skillsContent = document.querySelectorAll(".skills-content");
 const skillsHeader = document.querySelectorAll(".skills-header");
 const tabs = document.querySelectorAll("[data-target]");
 const tabContents = document.querySelectorAll("[data-content]");
+const sections = document.querySelectorAll("section[id]");
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 if (navToggle) {
   navToggle.addEventListener("click", () => {
@@ -34,16 +40,90 @@ function toggleSkills() {
     this.parentNode.className = "skills-content open";
   }
 }
-skillsHeader.forEach((skill) => skill.addEventListener("click", toggleSkills));
+skillsHeader.forEach((skill) => {
+  skill.addEventListener("click", toggleSkills);
+});
 
-tabs.forEach((tab) =>
+tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.target);
-    tabContents.forEach((tabContent) =>
-      tabContent.classList.remove("qualification-active"),
-    );
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification-active");
+    });
     target.classList.add("qualification-active");
-    tab.forEach((tab) => tab.classList.remove("qualification-active"));
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification-active");
+    });
     tab.classList.add("qualification-active");
-  }),
-);
+  });
+});
+
+let swiper = new Swiper(".portfolio-container", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    let sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollUp);
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme,
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme,
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});

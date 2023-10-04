6979f599e4be4b24aa7f76b17ae9e3e8f5edc144
memewei <minwei2002@gmail.com>
2023-10-04 20:30:30 +0800

    Add PHP to handle form data

---
+++ form-handling.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+  Name: <?php echo $_POST["name"]; ?><br>
+  Email address: <?php echo $_POST["email"]; ?><br>
+  Project: <?php echo $_POST["project"]; ?><br>
+  Message: <?php echo $_POST["message"]; ?><br>
+  </body>
+</html>

4f7361ad41f819a9c42a3d8623658d4320dea2d5
memewei <minwei2002@gmail.com>
2023-10-04 15:38:57 +0800

    Fix CSS typo

--- src/index.js
+++ src/index.js
@@ -72,27 +72,6 @@ let swiper = new Swiper(".portfolio-container", {
   },
 });
 
-function scrollActive() {
-  const scrollY = window.pageYOffset;
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    let sectionId = current.getAttribute("id");
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav-menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav-menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
-}
-
-window.addEventListener("scroll", scrollActive);
-
 function scrollHeader() {
   const nav = document.getElementById("header");
   if (this.scrollY >= 80) nav.classList.add("scroll-header");
